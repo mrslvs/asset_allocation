6c0f40d64718aa903401baf24b84879ef53f48f9
mrslvs <sekerka.miroslav@outlook.com>
2021-02-26 08:25:04 +0100

    added functions to evaluate conditions

--- asset_allocation.m
+++ asset_allocation.m
@@ -6,3 +6,31 @@ space = [space_down; space_up];
 pop_size = 200;
 
 population = genrpop(pop_size, space);
+
+function bool = first_cond(individual)
+    bool = 0;
+    if sum(individual) > 10000000
+        bool = 1;
+    end
+end
+
+function bool = second_cond(individual)
+    bool = 0;
+    if individual(1) + individual(2) > 2500000
+        bool = 1;
+    end
+end
+
+function bool = third_cond(individual)
+    bool = 0;
+    if individual(5) - individual(4) > 0
+        bool = 1;
+    end
+end
+
+function bool = fourth_cond(i)
+    bool = 0;
+    if (0.5 * (i(3) + i(4) - i(1) - i(2) - i(5))) > 0
+        bool = 1;
+    end
+end
