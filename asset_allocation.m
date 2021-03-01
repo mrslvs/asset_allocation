961162a2b6debe9691337ce9dc18173035021637
mrslvs <sekerka.miroslav@outlook.com>
2021-03-01 21:44:33 +0100

    added fitness with proportionate penalization

--- asset_allocation.m
+++ asset_allocation.m
@@ -22,13 +22,13 @@ vec_of_best_ones = [20, 15, 10];
 %end
 
 for i=1:cycles
-    profit(i,:) = fitness_three_fee(population, pop_size);    
-    best_individuals(i) = min(profit(i, :));
+    profit(i,:) = fitness_proportionate(population, pop_size);    
+    best_individuals(i) = max(profit(i, :));
     
-    temp_best = selbest(population, profit(i, :), vec_of_best_ones);
+    temp_best = selbest(population, -profit(i, :), vec_of_best_ones);
     
     %work = selsus(population, profit(i, :), 155);
-    work = seltourn(population, profit(i, :), 155);
+    work = seltourn(population, -profit(i, :), 155);
     
     %work = mutx(work, 0.8, space);
     work = crossov(work, 1, 0);
@@ -70,23 +70,44 @@ function [F] = fitness_three_fee(population, pop_size)
     end
 end
 
+function F = fitness_proportionate(population, pop_size)
+    for i=1:pop_size
+        fit = fitness(population(i, :));
+        cond_matrix = conditions(population(i, :));
+        
+        %penalties
+        fee = proportionate(population(i, :), cond_matrix);
+        F(i) = fit - fee; %F(max) = -F(min)
+    end
+end
+
 function fee = proportionate(i, cond_matrix)
 %umerna
     fee = 0;
     
+    if cond_matrix(1) == 1
+        x = sum(i) - 10000000;
+        fee = fee + x;
+    end
+    
+    if cond_matrix(2) == 1
+        x = (i(1) + i(2)) - 2500000;
+        fee = fee + x;
+    end
+    
     if cond_matrix(3) == 1
-        x = (i(5) - i(4)) ^2; %x4 must be bigger
+        x = i(5) - i(4); %x4 must be bigger
         fee = fee + x;
     end
     
     if cond_matrix(4) == 1
-        x = (0.5 * (i(3) + i(4) - i(1) - i(2) - i(5))) ^2;
+        x = 0.5 * (i(3) + i(4) - i(1) - i(2) - i(5));
         fee = fee + x;
     end
 end
 
 function fee = death_penalty()
-    fee = 1000000000;
+    fee = 100000000;
 end
 
 function fee = infringement_rate(cond_matrix)
